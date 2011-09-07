495eb82dd05ae30ed92736f7d9e853e501a6ab64
link <link@d393b7df-168a-48a4-b58c-f62163b6d633>
2011-09-07 19:42:10 +0000

    Dodany odczyt / zapis galerii do fs, dodany w miare rozsadny jak na teraz sposób tworzenia galerii.
    do zrobienia:
       -zmienić by odczyt zdjecia następował tylko w chwili gdy jest ono potrzebne (ograniczenie na dostepną pamięć ram)
       -pomyslec nad klasami
       -zastanowić sie może nad gui
       -suwaki do przeglądania zdjęć w galrii
       -rozpocząc obróbkę
       -zobaczyc jak sie uzywało pthread
       -poczytac o tym co mam dostępne w opencv
       -pomyslec o jakims mirrorze chociaz na rabie
    
    
    git-svn-id: svn+ssh://rab.ict.pwr.wroc.pl/home/morynicz/svn/INZ/trunk@8 d393b7df-168a-48a4-b58c-f62163b6d633

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <list>
 #include <cmath>
 #include <string>
-
+#include "filesystem.hpp"
 
 #include <sstream> //do nazw plików
 
@@ -16,11 +16,17 @@ using namespace cv;
 using namespace std;
 
 const float FACE_FACTOR=0.2;
+const string GALERIES="Galeries";
+const string LABEL="Label";
+const string COUNTER="COUNTER";
+const string ADDRES="Addres";
+
 
 struct Gallery{
   string label;
-  long counter;
-  list<string> photos;
+  int counter;
+  vector<string> photos;
+  vector<Mat> pictures;
 };
     
 
@@ -41,6 +47,7 @@ int main(int argc,char **argv){
   
   string adres;
 
+  list<Gallery> galeries; 
 
   long long counter=1;
 
@@ -57,15 +64,48 @@ int main(int argc,char **argv){
   szukacz.load(argv[1]);
   adres=argv[2];
 
+  try{
 
-  /*
+  
+
     FileStorage fs(string(adres+"/galeria.xml"),
 		   FileStorage::READ);
     if(!fs.isOpened()){
       cv::Exception err(1,"file cannot be opened",
 			__func__,__FILE__,__LINE__);
       throw err;
-      }*/
+    }
+    
+    //    FileNode n=fs.getFirstTopLevelNode();
+    FileNode gals=fs["Galeries"];
+    FileNodeIterator it=gals.begin();
+    
+    for(;it!=gals.end();++it){
+      Gallery gal;
+      (*it)[LABEL]>>gal.label;
+      (*it)[COUNTER]>>gal.counter;
+      
+      (*it)[ADDRES]>>gal.photos;
+      cerr<<gal.label<<endl<<gal.counter<<endl;
+      for(unsigned i=0;i<gal.photos.size();++i){
+	Mat img;
+	cerr<<gal.photos[i]<<endl;
+	img=imread(gal.photos[i]);
+	gal.pictures.push_back(img);
+      }
+      galeries.push_back(gal);
+      
+    }
+   
+    fs.release();
+   
+  } 
+
+  catch(Exception ex){
+    cerr<<ex.code<<endl<<ex.err<<endl<<ex.func<<endl<<ex.line<<endl;
+  }
+    
+
     // //=========================================================
     //    tu powinno byc wczytanie do wektora galerii 
     //   poszczegolnych galerii z fs'a
@@ -78,6 +118,7 @@ int main(int argc,char **argv){
   namedWindow("in",CV_WINDOW_NORMAL);
   namedWindow("proces",CV_WINDOW_NORMAL);
   namedWindow("test",CV_WINDOW_NORMAL);
+  namedWindow("gemba",CV_WINDOW_NORMAL);
   while(ster!='q'){
     try{
       kam.stopKlatka(obr);
@@ -95,20 +136,19 @@ int main(int argc,char **argv){
     }
     
     try{
-        
+      
       {
-	
-	//	fs<<"test"<<"[";
-
-
 	stringstream sBufor;
-	string cel,imie_nazwisko,buff;
+	string cel,label,buff;
 	szukacz.detectMultiScale(eq[3],twarze,1.3);
 	if(!twarze.empty()){
 	  m=floor(sqrt(twarze.size()));
 	  n=ceil(sqrt(twarze.size()));
 	  mid.create(rozm.width*m*(1+FACE_FACTOR),rozm.width*n,CV_8UC3);
 	  int i=0;
+	  
+	  list<Gallery>::iterator git;
+	  
 	  for(vector<Rect>::iterator it=twarze.begin();
 	      it!=twarze.end();++it,++i){
 	    it->y-=(it->height)*FACE_FACTOR/2;
@@ -117,41 +157,116 @@ int main(int argc,char **argv){
 		      Point(it->x,it->y),
 		      Point(it->x+it->width,it->y+it->height),
 		      Scalar(255,0,0));
-
+	    
 	    Mat midPt=mid(Rect(rozm.width*(i/n),
 			       rozm.width*(i%m)*(1+FACE_FACTOR),
 			       rozm.width,rozm.width*(1+FACE_FACTOR)));
 	    resize(Mat(obr,(*it)),midPt,midPt.size(),0,0,CV_INTER_LINEAR);
-	    cout<<"kto to?"<<endl;
-	    cin>>imie_nazwisko;
-	    if(imie_nazwisko=="koniec")
-	      break;
-	    sBufor<<adres<<'/'<<imie_nazwisko<<'/'<<counter++<<".jpg";
-	    sBufor>>cel;
-	    cerr<<cel<<endl;
 	    
-	    Mat toWrite(rozm.width,rozm.height,CV_8U);
-	    resize(Mat(eq[3],(*it)),toWrite,toWrite.size(),0,0,CV_INTER_LINEAR);
+	  
+	    
+	    imshow("gemba",midPt);
+	    if(ster!='q'){
+	      ster=' ';
+	      ster=waitKey(1000);
+	      if(ster=='c'){
+		cout<<"kto to?"<<endl;
+		cin>>label;
+		if(label=="koniec")
+		  break;
+		if(!galeries.empty()){
 
-	    imwrite(cel,toWrite);
+
+		  for(git=galeries.begin();
+		      git!=galeries.end();
+		      ++git){
+		    cerr<<"szuka "<<label<<" == "<<git->label<<'?'<<endl;
+		    if(git->label==label)
+		      break;
+		  }
+	      
+		  if(git==galeries.end()){
+		    Gallery gal;
+		    gal.label=label;
+		    gal.counter=0;
+		    --git;
+		    galeries.push_back(gal);
+		    ++git;
+		    if(!boost::filesystem::exists(adres+'/'+label)){
+		      boost::filesystem::create_directory(adres+'/'+label);
+		      cerr<<(adres+'/'+label)<<endl;
+		    }
+		    //git=galeries.back();
+		  }
+		}else{
+		  Gallery gal;
+		  gal.label=label;
+		  gal.counter=0;
+		  galeries.push_back(gal);
+		  git=galeries.begin();
+		  if(!boost::filesystem::exists(adres+'/'+label)){
+		    boost::filesystem::create_directory(adres+'/'+label);
+		    cerr<<(adres+'/'+label)<<endl;
+		  }
+		}
+
+		sBufor<<adres<<'/'<<git->label<<'/'<<git->counter++<<".jpg";
+		sBufor>>cel;
+		cerr<<cel<<endl;
 	    
-	    //	    fs<<cel;
+		Mat toWrite(rozm.width,rozm.height,CV_8U);
+		resize(Mat(eq[3],(*it)),toWrite,toWrite.size(),
+		       0,0,CV_INTER_LINEAR);
+
+		imwrite(cel,toWrite);
+	      
+		git->photos.push_back(cel);
+		git->pictures.push_back(toWrite.clone());
+	      
+	      }
+	    }
+	    if(label=="koniec")
+	      break;
 	  }
-	  if(imie_nazwisko=="koniec")
-	    break;
 	}
-	//	fs<<"]";
       }
-  
-      imshow("test",gemben);
-      imshow("proces",mid);
-      ster=waitKey(100);
-    
+      if(ster!='q'){
+	imshow("test",gemben);
+	imshow("proces",mid);
+	ster=waitKey(100);
+      }
     }
     catch(Exception ex){
       cerr<<ex.code<<endl<<ex.err<<endl<<ex.func<<endl<<ex.line<<endl;
     }
     
+  }
+
+  {
+    FileStorage fs(string(adres+"/galeria.xml"),
+		   FileStorage::WRITE);
+    if(!fs.isOpened()){
+      cv::Exception err(1,"file cannot be opened",
+			__func__,__FILE__,__LINE__);
+      throw err;
+    }
+
+
+    {
+      fs<<GALERIES<<"[";
+      for(list<Gallery>::iterator it=galeries.begin();
+	  it!=galeries.end();++it){
+	fs<<"{"<<LABEL<<it->label;
+	fs<<COUNTER<<it->counter;
+	fs<<ADDRES<<"[";
+	for(unsigned i=0;i<it->photos.size();++i){
+	  fs<<it->photos[i];
+	}
+	fs<<"]"<<"}";
+      }
+
+      fs<<"]";
+    }
   }
   //===================================================
   // a tu zapis wektora galerii do fs'a
